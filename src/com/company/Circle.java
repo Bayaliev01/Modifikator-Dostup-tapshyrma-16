1fd6f65d937cd889b33f99618de0342d8010d329
lesson1 <bayaliev07@gmail.com>
2022-07-15 17:39:26 +0300

    gfuk

--- src/com/company/Circle.java
+++ src/com/company/Circle.java
@@ -1,4 +1,15 @@
 package com.company;
 
-public class Cirsle {
+public class Circle {
+private static final double Pi = 3.1415;
+
+
+   public static void area(int a){
+    System.out.printf("%.4f",Pi*( a * a));
+       System.out.println(" ");
+}
+public static void circumference(int a){
+    System.out.printf("%.4f",Pi * 2 * a);
+}
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,8 @@
+package com.company;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+     Circle.area(5);
+     Circle.circumference(5);
     }
 }
